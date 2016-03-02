849b33b389dfe5ae005940575306eb72a497a485
Mira Scarvalone <mirascarvalone@Miras-MacBook-Pro.local>
2016-03-02 14:15:09 -0500

    Fix users_controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  include SessionsHelper
+
+  def new
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      log_in(user)
+      redirect_to posts_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :password, :password_confirmation)
+    end
+end
