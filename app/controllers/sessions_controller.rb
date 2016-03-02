6d9b9188a9850e0f4abcd6212c8aedb1efc46f58
Mira Scarvalone <mirascarvalone@Miras-MacBook-Pro.local>
2016-03-02 14:49:05 -0500

    Add pry to Gemfile

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    binding.pry
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
       log_in(user)
       redirect_to posts_path
     else
+      @error = 'There was an error signing in. Check that your username and password are correct.'
       render 'new'
     end
   end
