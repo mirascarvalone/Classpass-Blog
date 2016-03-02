792640a73958b6854a83d877770334cfbf7367d1
Mira Scarvalone <mirascarvalone@Miras-MacBook-Pro.local>
2016-03-02 15:23:22 -0500

    Add post creation in controllers and views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,22 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.new(post_params.merge(author: current_user))
+    if post.save
+      redirect_to post_path(post)
+    else
+      @error = "Your post could not save."
+      render 'new'
+    end
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
 end
