3656be626709360b970c1f9c25ad7e7793b7e407
Mira Scarvalone <mirascarvalone@Miras-MacBook-Pro.local>
2016-03-04 14:14:20 -0500

    Add studio dropdown to post creation (still need to add form to write in if it's new)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,18 @@
 class Post < ActiveRecord::Base
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
+  belongs_to :studio
+
   def author_username
     author.username
   end
 
+  def studio_name
+    studio.name
+  end
+
   validates :content, presence: true
   validates :title, presence: true
+  validates :studio, presence: true
 
 end
