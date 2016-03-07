100975ba9177ebe702c480ab1d9274c499d6143b
Mira Scarvalone <mirascarvalone@Miras-MacBook-Pro.local>
2016-03-07 16:30:43 -0500

    Revert to 50e06a7cb0d95ee52b2319abf2d20d9fd562c317

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -3,4 +3,14 @@ class StudiosController < ApplicationController
     @studios = Studio.all
   end
 
+  def show
+    @studio = Studio.find_by(params[:id])
+    # @map_data = Gmaps4rails.build_markers(@studio) do |studio, marker|
+    #  marker.lat studio.latitude
+    #  marker.lng studio.longitude
+    #  marker.infowindow studio.name
+    #  marker.json({name: studio.name})
+    # end
+  end
+
 end
